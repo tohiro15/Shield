97c32ca4808237ce5e03aa6dc919742a2d2bad13
Daniil <136441101+tohiro15@users.noreply.github.com>
2025-02-03 21:52:10 +0800

    feat: finalize stasis projectile
    #RU
    - Стазис снаряд доработан, осталось создать модельку.
    
    #ENG
    - Stasis projectile completed, only the model remains to be created.

--- Assets/Scripts/Other/StasisShell.cs
+++ Assets/Scripts/Other/StasisShell.cs
@@ -2,49 +2,98 @@ using UnityEngine;
 
 public class StasisShell : MonoBehaviour
 {
-    [SerializeField] private FireController _fireController;
-    [SerializeField] private Renderer _bulletRenderer;
+    private FireController _fireController;
+    private Renderer _bulletRenderer;
+    private SpriteRenderer _spriteRenderer;
 
     private bool _isPickup = false;
 
-    private void OnTriggerEnter(Collider other)
+    private void Awake()
     {
-        if (other.CompareTag("Player") || other.CompareTag("Shield"))
+        _fireController = FindObjectOfType<FireController>();
+
+        _bulletRenderer = GetComponent<Renderer>();
+        if (_bulletRenderer == null)
         {
-            if (_isPickup)
-            {
-                Debug.Log("Снаряд уже собран!");
-                return;
-            }
-            if (_fireController == null)
-            {
-                Debug.LogError("FireController не установлен!");
-                return;
-            }
+            _bulletRenderer = GetComponentInChildren<Renderer>();
+        }
 
-            if (_bulletRenderer == null)
+        if (_bulletRenderer == null)
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            if (_spriteRenderer == null)
             {
-                Debug.LogError("BulletRenderer не установлен!");
-                return;
+                _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
             }
+        }
 
-            _fireController.LoadBullet();
+        if (_fireController == null)
+        {
+            Debug.LogError("FireController не найден в сцене!");
+        }
 
-            if (_bulletRenderer.material != null)
-            {
-                Color newColor = _bulletRenderer.material.color;
-                newColor.a = 100 / 255f;
-                _bulletRenderer.material.color = newColor;
-            }
-            else
-            {
-                Debug.LogError("Материал не найден у BulletRenderer!");
-            }
-
-            _isPickup = true;
-
-            Debug.Log("Стазис снаряд собран!");
+        if (_bulletRenderer == null && _spriteRenderer == null)
+        {
+            Debug.LogError("Ни Renderer, ни SpriteRenderer не найдены на объекте или его дочерних объектах!");
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player") && !other.CompareTag("Shield"))
+        {
+            return;
+        }
+
+        if (_isPickup)
+        {
+            Debug.Log("Снаряд уже собран!");
+            return;
+        }
+
+        if (!ValidateComponents())
+        {
+            return;
+        }
+
+        CollectStasisShell();
+    }
+
+    private bool ValidateComponents()
+    {
+        if (_fireController == null)
+        {
+            Debug.LogError("FireController не установлен или не найден!");
+            return false;
+        }
+
+        if (_bulletRenderer == null && _spriteRenderer == null)
+        {
+            Debug.LogError("Ни BulletRenderer, ни SpriteRenderer не установлены или не найдены!");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void CollectStasisShell()
+    {
+        _fireController.LoadBullet();
+
+        if (_bulletRenderer != null)
+        {
+            Color newColor = _bulletRenderer.material.color;
+            newColor.a = 100 / 255f;
+            _bulletRenderer.material.color = newColor;
+        }
+        else if (_spriteRenderer != null)
+        {
+            Color newColor = _spriteRenderer.color;
+            newColor.a = 100 / 255f;
+            _spriteRenderer.color = newColor;
+        }
+
+        _isPickup = true;
+        Debug.Log("Стазис снаряд собран!");
+    }
 }
